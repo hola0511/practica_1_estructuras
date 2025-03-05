130ffb54c8da6cefc9459a0e3f551eb1c4b8c810
hola0511 <espinosaariassamuel@gmail.com>
2025-03-04 21:14:35 -0500

    cambios en documentos

--- logica/ecosistema.py
+++ logica/ecosistema.py
@@ -1,24 +1,42 @@
 import random
 from organismos import Depredador, Presa, Planta
 
+def inicializar_matriz(fila=0, col=0, n=10, matriz=None):
+    if matriz is None:
+        matriz = []
+    if fila == n:
+        return matriz
+    if col == 0:
+        matriz.append([])
+    matriz[fila].append(None)
+    if col == n - 1:
+        return inicializar_matriz(fila + 1, 0, n, matriz)
+    return inicializar_matriz(fila, col + 1, n, matriz)
 
-def crear_matriz(n, densidad_presas=0.1, densidad_depredadores=0.15, densidad_plantas=0.3):
-    matriz = [[None for _ in range(n)] for _ in range(n)]
+def generar_posiciones_recursivo(i=0, j=0, n=10, acumulador=None):
+    if acumulador is None:
+        acumulador = []
+    if i == n:
+        return acumulador
+    if j == n:
+        return generar_posiciones_recursivo(i + 1, 0, n, acumulador)
+    acumulador.append((i, j))
+    return generar_posiciones_recursivo(i, j + 1, n, acumulador)
 
-    def colocar_organismos_recursivo(posiciones, clase, cantidad):
-        if cantidad == 0 or not posiciones:
-            return
-        x, y = posiciones.pop()
-        matriz[x][y] = clase()
-        colocar_organismos_recursivo(posiciones, clase, cantidad - 1)
+def colocar_organismos_recursivo(matriz, posiciones, clase, cantidad):
+    if cantidad == 0 or not posiciones:
+        return
+    x, y = posiciones.pop()
+    matriz[x][y] = clase()
+    colocar_organismos_recursivo(matriz, posiciones, clase, cantidad - 1)
 
-    posiciones = random.sample([(i, j) for i in range(n) for j in range(n)], n * n)
-    colocar_organismos_recursivo(posiciones, Presa, int(n * n * densidad_presas))
-    colocar_organismos_recursivo(posiciones, Depredador, int(n * n * densidad_depredadores))
-    colocar_organismos_recursivo(posiciones, Planta, int(n * n * densidad_plantas))
-
+def crear_matriz(n=10, densidad_presas=0.1, densidad_depredadores=0.15, densidad_plantas=0.3):
+    matriz = inicializar_matriz(n=n)
+    posiciones = random.sample(generar_posiciones_recursivo(n=n), n * n)
+    colocar_organismos_recursivo(matriz, posiciones, Presa, int(n * n * densidad_presas))
+    colocar_organismos_recursivo(matriz, posiciones, Depredador, int(n * n * densidad_depredadores))
+    colocar_organismos_recursivo(matriz, posiciones, Planta, int(n * n * densidad_plantas))
     return matriz
-
 
 def imprimir_matriz(matriz):
     def imprimir_filas_recursivo(filas):
@@ -27,5 +45,4 @@ def imprimir_matriz(matriz):
             return
         print("[" + " ".join(str(c) if c else "." for c in filas[0]) + "]")
         imprimir_filas_recursivo(filas[1:])
-
     imprimir_filas_recursivo(matriz)
--- logica/simulacion.py
+++ logica/simulacion.py
@@ -1,7 +1,7 @@
 import random
 from movimiento import mover_presas, mover_depredadores
 from organismos import Planta
-from ecosistema import imprimir_matriz
+from ecosistema import imprimir_matriz, generar_posiciones_recursivo
 
 def regenerar_plantas(matriz, posiciones, cantidad):
     if cantidad == 0 or not posiciones:
@@ -14,7 +14,7 @@ def actualizar_ecosistema(matriz, ciclos, actual=0):
     if actual == ciclos:
         return
 
-    posiciones = [(i, j) for i in range(len(matriz)) for j in range(len(matriz))]
+    posiciones = generar_posiciones_recursivo(n=len(matriz))
     direcciones = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     random.shuffle(direcciones)
 
